361e16ae693c722246fa89049964d858a246c550
jenksy <jjenksy@gmail.com>
2020-09-24 11:14:09 -0500

    Initial Commit

---
+++ H4XOR New/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR New
+//
+//  Created by John Jenkins on 9/24/20.
+//  Copyright © 2020 John Jenkins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
